51593d82e0d0ef40174d2ef6a998e329cf5befe3
William Claros <will.claros.revollo@gmail.com>
2024-01-30 20:13:13 -0300

    New kata by Codewars platform solved

---
+++ src/test/java/katasTest/ConvertNumberToArrayTest.java
@@ -0,0 +1,22 @@
+/*
+ *   @(#)" ".java
+ *
+ *   Copyright (c) 2024 Training Foundation.
+ *   1945 Av America, Zona Norte, Cochabamba, Bolivia.
+ *   All rights reserved.
+ *
+ *   This software is the confidential and proprietary information of
+ *   Training Foundation, ("Confidential Information").  You shall not
+ *   disclose such Confidential Information and shall use it only in
+ *   accordance with the terms of the license agreement you entered into
+ *   with Training Foundation.
+ *
+ *   @author Will Claros
+ *   @version 26 January 2024
+ *
+ */
+
+package katasTest;
+
+public class ConvertNumberToArrayTest {
+}
